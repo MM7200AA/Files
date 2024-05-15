e7411e179c1af31c7d7a17734df89496872dff27
Sir_Abbas <167198584+Sir-Abbas08@users.noreply.github.com>
2024-05-16 02:04:56 +0300

    first commit

---
+++ Programs to calculate the sum of three numbers.py
@@ -0,0 +1,14 @@
+def calculate_sum():
+    # Get three numbers as input from the user
+    num1 = float(input("Enter First Number: "));
+    num2 = float(input("Enter Second Number: "));
+    num3 = float(input("Enter Third Number: "));
+    # Calculate the sum of the three numbers
+    total_sum = num1 + num2 + num3
+
+    # Print the result
+    print("The Sum of", num1, "+", num2, "+", num3, "is:", total_sum)
+
+    #Call the function to calculate the sum
+    calculate_sum()
+
